54dcb0b5d8b74ff05bd24883b05801ca95d64b08
Softwarica College Lab <softwarica@softwarica.com>
2019-06-16 11:21:18 +0545

    Services

--- wearable/app/src/main/java/com/example/wearable/MainActivity.java
+++ wearable/app/src/main/java/com/example/wearable/MainActivity.java
@@ -2,19 +2,31 @@ package com.example.wearable;
 
 import android.os.Bundle;
 import android.support.wearable.activity.WearableActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends WearableActivity {
-
-    private TextView mTextView;
+    private TextView tvoutput;
+    private Button btnshow;
+    private EditText etfirst;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextView = (TextView) findViewById(R.id.text);
+        tvoutput=findViewById(R.id.tvoutput);
+        etfirst=findViewById(R.id.etfirst);
+        btnshow=findViewById(R.id.btnshow);
 
+        btnshow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvoutput.setText(etfirst.getText().toString());
+            }
+        });
         // Enables Always-on
         setAmbientEnabled();
     }
